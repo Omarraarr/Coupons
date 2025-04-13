c68b9d69169a46e8fe9298a2aa434ced2f076b26
Omar <62112110+Omarraarr@users.noreply.github.com>
2025-04-13 06:07:56 +0200

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="min-h-screen flex items-center justify-center bg-transparent">
         <p className="text-lg font-medium text-main">Loading...</p>
       </div>
     </>
